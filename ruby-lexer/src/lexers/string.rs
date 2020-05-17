74aa8c8fa72aa034682b54401c7db7e96b083731
Kenaniah Cerny <kenaniah@gmail.com>
2020-05-17 13:05:42 -0700

    checkpoint

--- ruby-lexer/src/lexers/string.rs
+++ ruby-lexer/src/lexers/string.rs
@@ -27,17 +27,16 @@ pub fn single_quoted_escape_sequence(i: Input) -> StringResult {
 
 /// `\` *single_quoted_string_meta_character*
 pub fn single_escape_character_sequence(i: Input) -> StringResult {
-    stub_string(i)
+    let (i, char1) = char('\\')(i)?;
+    let (i, char2) = single_quoted_string_meta_character(i)?;
+    Ok((i, string_from_2_chars(char1, char2)))
 }
 
 /// `\` *single_quoted_string_non_escaped_character*
 pub fn single_quoted_string_non_escaped_character_sequence(i: Input) -> StringResult {
     let (i, char1) = char('\\')(i)?;
     let (i, char2) = single_quoted_string_non_escaped_character(i)?;
-    let mut string = String::with_capacity(char1.len_utf8() + char2.len_utf8());
-    string.push(char1);
-    string.push(char2);
-    Ok((i, string))
+    Ok((i, string_from_2_chars(char1, char2)))
 }
 
 /// `'` | `\`
@@ -67,6 +66,14 @@ fn stub_char(i: Input) -> CharResult {
     Err(nom::Err::Error((i, nom::error::ErrorKind::Char)))
 }
 
+/// Constructs a string from two characters
+fn string_from_2_chars(c1: char, c2: char) -> String {
+    let mut string = String::with_capacity(c1.len_utf8() + c2.len_utf8());
+    string.push(c1);
+    string.push(c2);
+    string
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -94,7 +101,22 @@ mod tests {
         assert_err!("foo");
         // Success cases
         assert_ok!("\\1", "\\1".to_owned());
+        assert_ok!("\\a", "\\a".to_owned());
         assert_ok!("\\東", "\\東".to_owned()); // U+6771: 'CJK Unified Ideograph-6771' "East"
+    }
+
+    #[test]
+    fn test_single_escape_character_sequence() {
+        use_parser!(single_escape_character_sequence, Input, String, ErrorKind);
+        // Parse errors
+        assert_err!("");
+        assert_err!("\\");
+        assert_err!("\\1");
+        assert_err!("\\a");
+        assert_err!("foo");
+        // Success cases
+        assert_ok!("\\'", "\\'".to_owned());
+        assert_ok!("\\\\", "\\\\".to_owned());
     }
 
 }
