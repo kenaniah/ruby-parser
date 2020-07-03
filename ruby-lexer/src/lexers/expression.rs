196b137cf00c2a3368bc88f643cf144820648c0f
Kenaniah Cerny <kenaniah@gmail.com>
2020-07-03 16:30:20 -0700

    checkpoint

--- ruby-lexer/src/lexers/expression.rs
+++ ruby-lexer/src/lexers/expression.rs
@@ -1,5 +1,7 @@
 use crate::{ExpressionResult, Input};
 
+/// *class_definition* | *singleton_class_definition* | *module_definition* | *method_definition* | *singleton_method_definition* | *yield_with_optional_argument* | *if_expression* | *unless_expression* | *case_expression* | *while_expression* | *until_expression* | *for_expression* | *return_without_argument* | *break_without_argument* | *next_without_argument* | *redo_expression* | *retry_expression* | *begin_expression* | *grouping_expression* | *variable_reference* | *scoped_constant_reference* | *array_constructor* | *hash_constructor* | *literal* | *defined_with_parenthesis* | *primary_method_invocation*
+/// NOTE: This was referred to as *primary-expression* in the ISO spec
 pub fn expression(i: Input) -> ExpressionResult {
     stub(i)
 }
