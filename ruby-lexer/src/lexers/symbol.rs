ceeb1783fb028dbd429ccb7b395f6215deccafee
Kenaniah Cerny <kenaniah@gmail.com>
2020-07-05 19:08:05 -0700

    checkpoint

--- ruby-lexer/src/lexers/symbol.rs
+++ ruby-lexer/src/lexers/symbol.rs
@@ -1,11 +1,13 @@
+use crate::lexers::identifier::identifier;
+use crate::lexers::keyword::keyword;
 use crate::lexers::string::double::double_quoted_string;
 use crate::lexers::string::single::single_quoted_string;
+use crate::lexers::token::operator_method_name;
+use crate::types::ParseResult;
 use crate::{Input, Interpolatable, Token, TokenResult};
 use nom::branch::alt;
-use nom::bytes::complete::tag;
-use nom::character::complete::{anychar, char, one_of};
-use nom::combinator::{map, recognize, verify};
-use nom::multi::many0;
+use nom::character::complete::char;
+use nom::combinator::{map, recognize};
 use nom::sequence::tuple;
 
 /// *symbol_literal* | *dynamic_symbol*
@@ -23,8 +25,9 @@ pub(crate) fn symbol_literal(i: Input) -> TokenResult {
 /// `:` *single_quoted_string*  | `:` *double_quoted_string* | `%s` *literal_beginning_delimiter* *non_expanded_literal_string** *literal_ending_delimiter*
 pub(crate) fn dynamic_symbol(i: Input) -> TokenResult {
     alt((
-        map(recognize(tuple((char(':'), single_quoted_string))), |s| {
-            Token::Symbol((*s).to_owned())
+        map(tuple((char(':'), single_quoted_string)), |mut t| {
+            t.1.insert(0, ':');
+            Token::Symbol(t.1)
         }),
         map(tuple((char(':'), double_quoted_string)), |t| match t.1 {
             Interpolatable::String(mut s) => {
@@ -40,10 +43,6 @@ pub(crate) fn dynamic_symbol(i: Input) -> TokenResult {
 }
 
 /// *identifier* | *operator_method_name* | *keyword*
-pub(crate) fn symbol_name(i: Input) -> TokenResult {
-    stub_token(i)
-}
-
-fn stub_token(i: Input) -> TokenResult {
-    Err(nom::Err::Error((i, crate::ErrorKind::Complete)))
+pub(crate) fn symbol_name(i: Input) -> ParseResult {
+    alt((recognize(identifier), operator_method_name, keyword))(i)
 }
