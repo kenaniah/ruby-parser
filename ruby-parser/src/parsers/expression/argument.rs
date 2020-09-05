4a174d8b0b8e9c6b26ef01c1d527d699c4ea1be9
Kenaniah Cerny <kenaniah@gmail.com>
2020-09-04 20:27:29 -0700

    checkpoint

--- ruby-parser/src/parsers/expression/argument.rs
+++ ruby-parser/src/parsers/expression/argument.rs
@@ -1,10 +1,14 @@
 use crate::lexer::*;
 use crate::parsers::expression::method::command;
 use crate::parsers::expression::object::association_list;
+use crate::parsers::expression::operator_expression;
 use crate::parsers::program::no_lt;
+use crate::parsers::program::ws;
 use nom::branch::alt;
 use nom::character::complete::char;
+use nom::combinator::map;
 use nom::combinator::{opt, recognize};
+use nom::multi::many0;
 use nom::sequence::tuple;
 
 /// *command* | *operator_expression_list* ( [ no line terminator here ] `,` )? | *operator_expression_list* ( [ no line terminator here ] `,` *splatting_argument* ) | *association_list* ( [ no line terminator here ] `,` )? | *splatting_argument*
@@ -30,7 +34,19 @@ pub(crate) fn splatting_argument(i: Input) -> NodeResult {
 
 /// *operator_expression* ( [ no line terminator here ] `,` *operator_expression* )*
 pub(crate) fn operator_expression_list(i: Input) -> NodeListResult {
-    stub_list(i)
+    map(
+        tuple((
+            operator_expression,
+            many0(map(
+                tuple((no_lt, char(','), ws, operator_expression)),
+                |t| t.3,
+            )),
+        )),
+        |(first, mut vec)| {
+            vec.insert(0, first);
+            vec
+        },
+    )(i)
 }
 
 /// `()` | `(` *argument_list* `)` | `(` *operator_expression_list* [ no line terminator here ] `,` *chained_command_with_do_block* `)` | `(` *chained_command_with_do_block* `)`
@@ -46,4 +62,24 @@ pub(crate) fn argument_list(i: Input) -> NodeListResult {
 /// `&` *operator_expression*
 pub(crate) fn block_argument(i: Input) -> NodeResult {
     stub(i)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_operator_expression_list() {
+        use_parser!(operator_expression_list);
+        // Parse errors
+        assert_err!("");
+        assert_err!("if");
+        assert_err!("1, 2\n, 3");
+        // Success cases
+        assert_ok!("hi", vec![Node::ident("hi", IdentifierKind::LocalVariable)]);
+        assert_ok!(
+            "1, 2,\n3",
+            vec![Node::integer(1), Node::integer(2), Node::integer(3)]
+        );
+    }
 }
