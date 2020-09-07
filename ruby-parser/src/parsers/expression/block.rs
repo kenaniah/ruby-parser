6b7170115a270fdd1ef4953d7f462b5d6e85412a
Kenaniah Cerny <kenaniah@gmail.com>
2020-09-06 21:46:07 -0700

    checkpoint

--- ruby-parser/src/parsers/expression/block.rs
+++ ruby-parser/src/parsers/expression/block.rs
@@ -1,31 +1,45 @@
 use crate::lexer::*;
+use crate::parsers::expression::assignment::multiple::left_hand_side;
+use crate::parsers::expression::assignment::multiple::multiple_left_hand_side;
+use crate::parsers::program::compound_statement;
 
 /// *brace_block* | *do block*
 pub(crate) fn block(i: Input) -> NodeResult {
-    stub(i)
+    alt((brace_block, do_block))(i)
 }
 
-/// `{` *block_parameter*? *block_body* }
+/// `{` *block_parameter*? *block_body* `}`
 pub(crate) fn brace_block(i: Input) -> NodeResult {
-    stub(i)
+    map(
+        tuple((char('{'), opt(block_parameter), block_body, char('}'))),
+        |t| Node::Placeholder,
+    )(i)
 }
 
 /// `do` *block_parameter*? *block_body* `end`
 pub(crate) fn do_block(i: Input) -> NodeResult {
-    stub(i)
+    map(
+        tuple((tag("do"), opt(block_parameter), block_body, ws, tag("end"))),
+        |t| Node::Placeholder,
+    )(i)
 }
 
 /// `| |` | `||` | `|` *block_parameter_list* `|`
 pub(crate) fn block_parameter(i: Input) -> NodeResult {
-    stub(i)
+    alt((
+        map(alt((tag("| |"), tag("||"))), |t| Node::Placeholder),
+        map(tuple((char('|'), block_parameter_list, char('|'))), |t| {
+            Node::Placeholder
+        }),
+    ))(i)
 }
 
 /// *left_hand_side* | *multiple_left_hand_side*
 pub(crate) fn block_parameter_list(i: Input) -> NodeResult {
-    stub(i)
+    alt((left_hand_side, multiple_left_hand_side))(i)
 }
 
 /// *compound_statement*
 pub(crate) fn block_body(i: Input) -> NodeResult {
-    stub(i)
+    compound_statement(i)
 }
