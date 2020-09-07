38edf69942f8892f7c2cfe4b0afc9e49899374d5
Kenaniah Cerny <kenaniah@gmail.com>
2020-09-06 21:54:49 -0700

    checkpoint

--- ruby-parser/src/parsers/expression/iteration.rs
+++ ruby-parser/src/parsers/expression/iteration.rs
@@ -1,26 +1,55 @@
 use crate::lexer::*;
+use crate::parsers::expression::assignment::multiple::left_hand_side;
+use crate::parsers::expression::assignment::multiple::multiple_left_hand_side;
+use crate::parsers::expression::expression;
+use crate::parsers::program::compound_statement;
+use crate::parsers::program::separator;
 
 /// `while` *expression* *do_clause* `end`
 pub(crate) fn while_expression(i: Input) -> NodeResult {
-    stub(i)
+    map(
+        tuple((tag("while"), expression, do_clause, tag("end"))),
+        |t| Node::Placeholder,
+    )(i)
 }
 
 /// *separator* *compound_statement* | [ no line terminator here ] `do` *compound_statement*
 pub(crate) fn do_clause(i: Input) -> NodeResult {
-    stub(i)
+    alt((
+        map(tuple((separator, compound_statement)), |t| {
+            Node::Placeholder
+        }),
+        map(tuple((no_lt, tag("do"), compound_statement)), |t| {
+            Node::Placeholder
+        }),
+    ))(i)
 }
 
 /// `until` *expression* *do_clause* `end`
 pub(crate) fn until_expression(i: Input) -> NodeResult {
-    stub(i)
+    map(
+        tuple((tag("until"), expression, do_clause, tag("end"))),
+        |t| Node::Placeholder,
+    )(i)
 }
 
 /// `for` *for_variable* [ no line terminator here ] `in` *expression* *do_clause* `end`
 pub(crate) fn for_expression(i: Input) -> NodeResult {
-    stub(i)
+    map(
+        tuple((
+            tag("for"),
+            for_variable,
+            no_lt,
+            tag("in"),
+            expression,
+            do_clause,
+            tag("end"),
+        )),
+        |t| Node::Placeholder,
+    )(i)
 }
 
 /// *left_hand_side* | *multiple_left_hand_side*
 pub(crate) fn for_variable(i: Input) -> NodeResult {
-    stub(i)
+    alt((left_hand_side, multiple_left_hand_side))(i)
 }
