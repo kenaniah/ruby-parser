cd435b3c5f152c782002b1f5a36364e06eea6a9d
Kenaniah Cerny <kenaniah@gmail.com>
2020-09-19 12:38:28 -0700

    checkpoint

--- ruby-parser/src/ast/statement.rs
+++ ruby-parser/src/ast/statement.rs
@@ -10,3 +10,17 @@ pub struct Alias {
 pub struct Undef {
     pub list: Vec<Identifier>,
 }
+
+#[derive(Debug, PartialEq)]
+pub struct Rescue {
+    pub body: Box<Node>,
+    pub rescue: Vec<RescueClause>,
+    pub otherwise: Box<Node>,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct RescueClause {
+    pub exceptions: Vec<Node>,
+    pub assigned_to: Box<Node>,
+    pub then: Box<Node>,
+}
