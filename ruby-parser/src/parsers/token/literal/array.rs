042b3ee1aff0730fa86407d626e104a563e6d5df
Kenaniah Cerny <kenaniah@gmail.com>
2020-10-05 19:43:38 -0700

    checkpoint

--- ruby-parser/src/parsers/token/literal/array.rs
+++ ruby-parser/src/parsers/token/literal/array.rs
@@ -1,4 +1,6 @@
 use crate::lexer::*;
+use crate::parsers::program::{line_terminator, whitespace};
+use crate::parsers::token::literal::string::double::double_escape_sequence;
 
 /// *quoted_non_expanded_array_constructor* | *quoted_expanded_array_constructor*
 pub(crate) fn array_literal(i: Input) -> NodeResult {
@@ -27,7 +29,7 @@ pub(crate) fn quoted_array_item_separator_list(i: Input) -> LexResult {
 
 /// *whitespace* | *line_terminator*
 pub(crate) fn quoted_array_item_separator(i: Input) -> LexResult {
-    stub_p(i)
+    alt((whitespace, line_terminator))(i)
 }
 
 /// *non_expanded_array_item_character*+
@@ -76,13 +78,19 @@ pub(crate) fn expanded_array_item_character(i: Input) -> LexResult {
 }
 
 /// *source_character* **but not** ( *quoted_array_item_separator* | `\` | `#` )
-pub(crate) fn non_escaped_array_item_character(i: Input) -> LexResult {
-    stub_p(i)
+pub(crate) fn non_escaped_array_item_character(i: Input) -> CharResult {
+    let (i, _) = peek(not(quoted_array_item_separator))(i)?;
+    none_of("\\#")(i)
 }
 
 /// *double_escape_sequence* | `\` *quoted_array_item_separator*
-pub(crate) fn expanded_array_escape_sequence(i: Input) -> LexResult {
-    stub_p(i)
+pub(crate) fn expanded_array_escape_sequence(i: Input) -> StringResult {
+    alt((
+        double_escape_sequence,
+        map(preceded(char('\\'), quoted_array_item_separator), |s| {
+            s.into()
+        }),
+    ))(i)
 }
 
 fn stub_p(i: Input) -> LexResult {
