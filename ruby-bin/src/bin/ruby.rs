3e5352e3752fb9bd2bd04bbd921da97ab22f26e0
Kenaniah Cerny <kenaniah@gmail.com>
2020-10-20 20:52:16 -0700

    checkpoint

--- ruby-bin/src/bin/ruby.rs
+++ ruby-bin/src/bin/ruby.rs
@@ -1,4 +1,8 @@
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+use std::path::Path;
+
 use clap::{App, Arg};
+use ruby_parser;
 
 fn argspec() -> App<'static> {
     App::new("ruby")
@@ -25,9 +29,17 @@ fn argspec() -> App<'static> {
         .arg(Arg::new("PROGRAM_FILENAME").about("path to a ruby file to be executed"))
 }
 
-fn main() {
+fn main() -> Result<(), Error> {
     let opts = argspec().get_matches();
-    println!("{:?}", opts);
+    //println!("{:?}", opts);
+    if let Some(path) = opts.value_of("PROGRAM_FILENAME") {
+        let path = Path::new(path);
+        let contents = std::fs::read_to_string(path)?;
+        let input = ruby_parser::lexer::Input::new(&contents);
+        let program = ruby_parser::parse(input);
+        println!("program {:?}", program);
+    }
+    Ok(())
 }
 
 #[cfg(test)]
